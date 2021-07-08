ac91322faea6263164270fb5c713c04e4cea9d52
Tristan Penman <tristan@tristanpenman.com>
2021-07-08 20:32:00 +1000

    Update bubble-down heap example

--- heaps/BubbleDownHeap.cpp
+++ heaps/BubbleDownHeap.cpp
@@ -3,16 +3,20 @@
 
 #include "BubbleDownHeap.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   std::vector<std::string> args;
   args.reserve(argc);
   for (int i = 1; i < argc; i++) {
     args.push_back(argv[i]);
   }
+
   BubbleDownHeap<std::string> heap(args.data(), argc);
+
   while (heap.getSize() > 0) {
     auto val = heap.pop();
     std::cout << val << std::endl;
   }
+
   return 0;
 }
--- heaps/BubbleDownHeap.h
+++ heaps/BubbleDownHeap.h
@@ -1,18 +1,20 @@
-#ifndef __HEAP_H
-#define __HEAP_H
+#pragma once
 
 #include <cstring>
 #include <iostream>
 
 template<typename T>
-void bubble_down(T* elements, size_t size, size_t target) {
+void bubble_down(T* elements, size_t size, size_t target)
+{
   size_t min = target;
+
   for (int i = 1; i <= 2; i++) {
     size_t p = target * 2 + i;
     if (p < size && elements[p] > elements[min]) {
       min = p;
     }
   }
+
   if (target != min) {
     T tmp = elements[target];
     elements[target] = elements[min];
@@ -22,14 +24,16 @@ void bubble_down(T* elements, size_t size, size_t target) {
 }
 
 template<typename T>
-void make_heap(T* elements, size_t size) {
+void make_heap(T* elements, size_t size)
+{
   for (int i = int(size) - 1; i >= 0; i--) {
     bubble_down(elements, size, i);
   }
 }
 
 template<typename T>
-class BubbleDownHeap {
+class BubbleDownHeap
+{
 public:
   BubbleDownHeap(const T* inputArray, size_t length)
     : elements(0)
@@ -46,47 +50,52 @@ public:
     make_heap(elements, size);
   }
 
-  ~BubbleDownHeap() {
+  ~BubbleDownHeap()
+  {
     delete[] elements;
   }
 
-  BubbleDownHeap(const BubbleDownHeap<T>& other) {
+  BubbleDownHeap(const BubbleDownHeap<T>& other)
+  {
     elements = new T[other.capacity];
     memcpy(elements, other.elements, sizeof(T) * other.capacity);
     size = other.size;
     capacity = other.capacity;
   }
 
-  BubbleDownHeap(BubbleDownHeap&& other) {
+  BubbleDownHeap(BubbleDownHeap&& other)
+  {
     elements = other.elements;
     other.elements = NULL;
   }
 
-  size_t getSize() const {
+  size_t getSize() const
+  {
     return size;
   }
 
-  size_t getCapacity() const {
+  size_t getCapacity() const
+  {
     return capacity;
   }
 
-  T pop() {
+  T pop()
+  {
     if (size == 0) {
       throw;
     }
+
     T result = elements[0];
     elements[0] = elements[size-1];
     size--;
     bubble_down(elements, size, 0);
+
     return result;
   }
 
 private:
-
   T* elements;
 
   size_t size;
   size_t capacity;
 };
-
-#endif
