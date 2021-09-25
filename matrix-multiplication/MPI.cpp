9724c5ca105541febd890f532112de8a57d50695
Tristan Penman <tristan@tristanpenman.com>
2021-09-25 11:55:16 +1000

    Add MPI example, more tidy up of README and other examples

---
+++ matrix-multiplication/MPI.cpp
@@ -0,0 +1,184 @@
+// #define DEBUG
+
+#include <cassert>
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+#include <mpi.h>
+
+#include "Matrix.h"
+
+using namespace std;
+using namespace std::chrono;
+
+template<typename T>
+void multiply_matrices(const Matrix<T> &matrix_a, const Matrix<T> &matrix_b, Matrix<T> &matrix_c)
+{
+  // check input matrix sizes
+  const auto n_a = matrix_a.columns();
+  const auto m_b = matrix_b.rows();
+  assert(matrix_a.columns() == matrix_b.rows());
+
+  // check output matrix size
+  const auto m_a = matrix_a.rows();
+  const auto n_b = matrix_b.columns();
+  assert(matrix_c.rows() == matrix_a.rows());
+  assert(matrix_c.columns() == matrix_b.columns());
+
+  // fill output matrix
+  for (int m = 0; m < m_a; m++) {
+    for (int n = 0; n < n_b; n++) {
+
+      // find value of cell [m,n]
+      T sum = 0;
+      for (int i = 0; i < n_a; i++) {
+        sum += matrix_a.get(m, i) * matrix_b.get(i, n);
+      }
+
+      // store value
+      matrix_c.set(m, n, sum);
+    }
+  }
+}
+
+int usage(char **argv)
+{
+  cout << endl;
+  cout << "Usage:" << endl;
+  cout << "  " << argv[0] << " <M1> <N1/M2> <N2> [seed]" << endl;
+  cout << endl;
+  cout << "Multiples a random M1xN1 matrix by a random M2xN2 matrix" << endl;
+
+  return 1;
+}
+
+int main(int argc, char **argv)
+{
+  if (argc == 1) {
+    return usage(argv);
+  }
+
+  if (argc != 4 && argc != 5) {
+    cout << "Invalid argument count: " << argc << endl;
+    return usage(argv);
+  }
+
+  int m_a = atoi(argv[1]);
+  if (m_a <= 0) {
+    cout << "Argument <M1> is invalid" << endl;
+    return usage(argv);
+  }
+
+  int n_a = atoi(argv[2]);
+  if (n_a <= 0) {
+    cout << "Argument <N1/M2> is invalid" << endl;
+    return usage(argv);
+  }
+
+  int n_b = atoi(argv[3]);
+  if (n_b <= 0) {
+    cout << "Argument <N2> is invalid" << endl;
+    return usage(argv);
+  }
+
+  MPI_Init(NULL, NULL);
+
+  // how many processes are there?
+  int cluster_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &cluster_size);
+
+  // who am I?
+  int host_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &host_rank);
+  
+  // matrices that are only used on the root node
+  Matrix<double> matrix_a(m_a, n_a);
+  Matrix<double> matrix_c(m_a, n_b);
+
+  // matrices that are used on all nodes
+  Matrix<double> matrix_a_partition(m_a / cluster_size, n_a);
+  Matrix<double> matrix_b(n_a, n_b);
+  Matrix<double> matrix_c_partition(m_a / cluster_size, n_b);
+
+  if (host_rank == 0) {
+    cout << "Cluster size: " << cluster_size << endl;
+
+    // parse random seed
+    std::optional<int> seed;
+    if (argc == 5) {
+      seed = atoi(argv[4]);
+      cout << "Random seeds: " << *seed << ", " << (*seed + 1) << endl;
+    }
+
+    // generate random data on the root node
+    matrix_a.randomise(-100, 100, seed);
+
+    if (seed) {
+      seed = *seed + 1;
+    }
+
+    matrix_b.randomise(-100, 100, seed);
+
+#ifdef DEBUG
+    cout << endl;
+    cout << "Matrix A:" << endl;
+    cout << matrix_a << endl;
+    cout << "Matrix B:" << endl;
+    cout << matrix_b << endl;
+#endif
+  }
+
+  auto start = high_resolution_clock::now();
+
+  // processes only need a subset of rows from matrix A, corresponding to their output rows
+  MPI_Scatter(
+      matrix_a.data(),              // address of send buffer (root node)
+      m_a * n_a / cluster_size,     // number of elements sent to each process (root node)
+      MPI_DOUBLE,                   // data type of send buffer elements (root node)
+      matrix_a_partition.data(),    // address of receive buffer
+      m_a * n_a / cluster_size,     // number of elements in receive buffer
+      MPI_DOUBLE,                   // data type of receive buffer elements
+      0,                            // ranking of sending process
+      MPI_COMM_WORLD);              // communicator
+
+  // all processes need all data from matrix B
+  MPI_Bcast(
+      matrix_b.data(),              // starting address of buffer
+      n_a * n_b,                    // number of entries in buffer
+      MPI_DOUBLE,                   // data type of buffer
+      0,                            // rank of broadcast root
+      MPI_COMM_WORLD);              // communicator
+
+  // do the work
+  multiply_matrices(matrix_a_partition, matrix_b, matrix_c_partition);
+
+  // gather the results
+  MPI_Gather(
+      matrix_c_partition.data(),    // starting address of send buffer
+      m_a * n_b / cluster_size,     // number of elements in send buffer
+      MPI_DOUBLE,                   // data type of send buffer elements
+      matrix_c.data(),              // starting address of receive buffer (root node)
+      m_a * n_b / cluster_size,     // number of elements for any single receive (root node)
+      MPI_DOUBLE,                   // data type of receive buffer elements (root node)
+      0,                            // ranking of receiving process
+      MPI_COMM_WORLD);              // communicator
+
+  auto stop = high_resolution_clock::now();
+
+  // only display complete results on the root node
+  if (host_rank == 0) {
+#ifdef DEBUG
+    cout << "Matrix C:" << endl;
+    cout << matrix_c << endl;
+#endif
+
+    // how long did it take?
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Duration: " << duration.count() << " microseconds (" << (double(duration.count()) / 1000000.0f) << " seconds)" << endl;
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
--- matrix-multiplication/README.md
+++ matrix-multiplication/README.md
@@ -2,24 +2,34 @@
 
 A comparison of different approaches to matrix multiplication.
 
-### Compiling
+## Contents
 
-All examples can be compiled using the included Makefile:
+The examples are grouped into three sections:
+
+* [Basic Examples](#basic-examples) - These are simple (single-threaded) examples explore the basic algorithms for matrix multiplication
+* [Multithreaded Examples](#multithreaded-examples) - These examples make use of multithreading to perform matrix multiplication across multiple cores on a single machine
+* [Advanced Examples](#advanced-examples) - These examples make use of MPI to distribute work across multiple nodes
+
+## Compiling
+
+The Basic and Multithreaded Examples do not have external dependencies (other than a C++17 compiler), and can all be compiled using the included Makefile:
 
     make
 
 When run without any command line arguments, all examples will print usage information:
 
-    $ ./Sequential1
+    $ ./Sequential
 
     Usage:
-      ./Sequential1 <M1> <N1/M2> <N2> [seed]
+      ./Sequential <M1> <N1/M2> <N2> [seed]
 
     Multiplies a random M1xN1 matrix by a random M2xN2 matrix
 
 Arguments between `<>` are required, while those between `[]` are optional.
 
-## Examples
+The Advanced Examples, which use MPI, must be compiled using their own `make` commands. These steps are documented below.
+
+## Basic Examples
 
 ### Sequential - Naive implementation
 
@@ -28,6 +38,14 @@ This is the simplest case, using a basic O(n^3) matrix multiplication algorithm.
 ### Recursive Case 1 - Divide and conquer
 
 This example uses recursion to break matrix multiplication into smaller sub-problems. It recursively multiplies, and then sums, sub-blocks of the input matrices. This is also O(n^3), but in practice, the additional function call overhead and cost memory copies makes this slower than the naive sequential algorithm.
+
+In order to handle rectangular matrices, the recursive implementations use larger square matrices to perform the multiplication. The size of these matrices is also rounded up to a power of two, to ensure that the work can be evenly divided. A smaller 'slice' of the output matrix is then used to display the result.
+
+### Recursive Case 2 - Strassen's algorithm
+
+The next algorithm is [Strassen's algorithm](https://en.wikipedia.org/wiki/Strassen_algorithm), which is an O(n^log2(7)) algorithm for matrix multiplication. The lower time bound is achieved by reducing the number of sub-block multiplications from 8 to 7, while increasing the number of additions.
+
+## Multithreaded Examples
 
 ### Multithreaded Case 1 - One cell per thread
 
@@ -44,3 +62,48 @@ In the previous two examples, the number of threads was tied to the size of the 
 Instead of creating one thread for each piece of work to be completed, we can instead use a fixed number of worker threads, specified as a command line argument. Then, each piece of work can be represented as a task in a queue, and the worker threads fetch tasks from the queue until there are none remaining.
 
 As in case 2, the number of rows per task is specified using a command line argument - this determines the size of each task. To govern access to the queue, we use a simple mutex.
+
+## Advanced Examples
+
+### MPI
+
+This example distributes computation across multiple nodes using [MPI](https://en.wikipedia.org/wiki/Message_Passing_Interface). Similar to the second multithreaded example, the task of calculating of a fixed number of rows is given to each node. The exact number of rows is determined based on the number of nodes that are available.
+
+To compile and run this example, you will need to install an MPI implementation, such as [Open MPI](https://www.open-mpi.org/) or [MPICH](https://www.mpich.org/). On Mac, Open MPI can be installed using Homebrew:
+
+    brew install open-mpi
+
+On Linux, you'll need to find an appropriate MPI dev package in your package manager.
+
+To compile the example, you will need to run `make` separately (this will use `mpic++` to compile the program):
+
+    make MPI
+
+Although you could run this example as-is, to see it work across multiple processes you will need to use `mpirun`:
+
+    mpirun -n 2 ./MPI 8 8 8
+
+Here the option `-n 2` determines how many processes to spawn. Here is example output when multiplying two 4x4 matrices (with the `DEBUG` option enabled):
+
+    Cluster size: 2
+    Random seeds: 0, 1
+
+    Matrix A:
+    18.5689 68.8531 71.5891 69.4503
+    24.7127 -23.1237 -40.4931 -88.6574
+    -45.4687 -4.46698 62.4337 -4.00457
+    -21.443 67.2158 -32.5208 29.6344
+
+    Matrix B:
+    99.437 86.5115 -74.3751 99.8081
+    -52.7822 -20.6839 -22.4179 33.9492
+    87.1078 69.2622 -37.3453 4.90963
+    -11.3094 -54.0846 6.88278 82.7924
+
+    Matrix C:
+    3662.75 1384.5 -5120.11 10292.3
+    1153.28 4606.58 -417.613 -5857.46
+    1198.26 699.709 1122.71 -4714.82
+    -8847.99 -7100.57 1506.46 2435.57
+
+    Duration: 158 microseconds (0.000158 seconds)
